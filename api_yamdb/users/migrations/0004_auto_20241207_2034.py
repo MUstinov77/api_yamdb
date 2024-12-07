dac51f1818c9ed0b0bd40769d0b706f488887527
Sergey Makarov <sergejmakarov30043@gmail.com>
2024-12-08 02:03:12 +0300

    Merge user-model and fix migrations

--- api_yamdb/users/migrations/0004_auto_20241207_2034.py
+++ api_yamdb/users/migrations/0004_auto_20241207_2034.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0003_alter_user_username'),
+        ('users', '0001_initial'),
     ]
 
     operations = [
