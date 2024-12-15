327c250145aba29a8eef2af9e8c8ce70b0aef9d2
MUstinov77 <m.ustinov2001@yandex.ru>
2024-12-15 12:52:08 +0300

    fix

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -17,12 +17,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework_simplejwt',
     'rest_framework',
     'api',
     'reviews',
     'users',
     'django_filters',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
